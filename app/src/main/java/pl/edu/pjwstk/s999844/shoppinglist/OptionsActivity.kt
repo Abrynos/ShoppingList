24a8a7832cca9f67408286329dae250e76f97c0a
Abrynos <6608231+Abrynos@users.noreply.github.com>
2021-02-17 12:30:47 +0100

    Add version information and links to releases on GitHub to options activity

--- app/src/main/java/pl/edu/pjwstk/s999844/shoppinglist/OptionsActivity.kt
+++ app/src/main/java/pl/edu/pjwstk/s999844/shoppinglist/OptionsActivity.kt
@@ -1,5 +1,7 @@
 package pl.edu.pjwstk.s999844.shoppinglist
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
@@ -7,6 +9,16 @@ import kotlinx.android.synthetic.main.activity_options.*
 import pl.edu.pjwstk.s999844.shoppinglist.settings.Settings
 
 class OptionsActivity : AppCompatActivity() {
+	companion object {
+		private const val RELEASES_PAGE_LINK = "https://github.com/Abrynos/ShoppingList/releases/"
+
+		private const val CURRENT_RELEASE_LINK = RELEASES_PAGE_LINK + "tag/" + BuildConfig.VERSION_NAME
+		private const val LATEST_RELEASE_LINK = RELEASES_PAGE_LINK + "latest"
+
+		private val CURRENT_RELEASE_URI by lazy { Uri.parse(CURRENT_RELEASE_LINK) }
+		private val LATEST_RELEASE_URI by lazy { Uri.parse(LATEST_RELEASE_LINK) }
+	}
+
 	private val settings: Settings by lazy { Settings(this) }
 
 	override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,4 +40,12 @@ class OptionsActivity : AppCompatActivity() {
 		settings.darkThemeActive = isDark
 		ThemeManager.setDark(isDark)
 	}
+
+	@Suppress("UNUSED_PARAMETER")
+	fun onClickCurrentVersion(view: View) = openUriInBrowser(CURRENT_RELEASE_URI)
+
+	@Suppress("UNUSED_PARAMETER")
+	fun onClickLatestRelease(view: View) = openUriInBrowser(LATEST_RELEASE_URI)
+
+	private fun openUriInBrowser(uri: Uri) = startActivity(Intent(Intent.ACTION_VIEW, uri))
 }
