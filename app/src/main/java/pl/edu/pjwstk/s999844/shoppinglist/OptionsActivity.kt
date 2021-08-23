dd388eeb9c6c4af596923eceebc6585a2734d988
Abrynos <6608231+Abrynos@users.noreply.github.com>
2021-08-23 13:04:19 +0200

    Re-license under GPL verion 2 and Add copyright notice
    If you have a problem with this change: You don't have to use this app and you don't have to contribute to it.
    But can you do this this easily? As translatable strings are not part of the source code and there have yet to be any contributions to the source code by other people, I am currently the only copyright holder and thus can re-license the project at any point in time without asking you for permission.

--- app/src/main/java/pl/edu/pjwstk/s999844/shoppinglist/OptionsActivity.kt
+++ app/src/main/java/pl/edu/pjwstk/s999844/shoppinglist/OptionsActivity.kt
@@ -1,3 +1,29 @@
+/*
+ *  _____ _                       _             _     _     _
+ * /  ___| |                     (_)           | |   (_)   | |
+ * \ `--.| |__   ___  _ __  _ __  _ _ __   __ _| |    _ ___| |_
+ *  `--. \ '_ \ / _ \| '_ \| '_ \| | '_ \ / _` | |   | / __| __|
+ * /\__/ / | | | (_) | |_) | |_) | | | | | (_| | |___| \__ \ |_
+ * \____/|_| |_|\___/| .__/| .__/|_|_| |_|\__, \_____/_|___/\__|
+ *                   | |   | |             __/ |
+ *                   |_|   |_|            |___/
+ *
+ * Copyright (C) 2021-2021  Sebastian Göls
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License only
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 package pl.edu.pjwstk.s999844.shoppinglist
 
 import android.content.Intent
